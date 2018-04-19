af27c4fc079bb185c21975f4d7a2431c1a4da828
Kishan B <kishanb_uk@hotmail.com>
2018-04-19 09:29:56 +0100

    first commit

---
+++ hw_requests_start/public/js/app.js
@@ -0,0 +1,39 @@
+let beerList = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+ const url = 'https://api.punkapi.com/v2/beers';
+ makeRequest(url, assignBeerData);
+});
+
+const assignBeerData = function() {
+ if (this.status !== 200) return;
+ const jsonString = this.responseText;
+ const beers = JSON.parse(jsonString);
+ beersList = beers;
+
+ const button = document.querySelector('#displayButton');
+ button.addEventListener('click', () => {
+   populateList(beersList);
+ });
+}
+
+const makeRequest = function (url, callback) {
+ const request = new XMLHttpRequest();
+ request.open('GET', url);
+ request.send();
+ request.addEventListener('load', callback);
+}
+
+const populateList = function(beers){
+ const ul = document.querySelector('#beer-list')
+
+ beers.forEach((beer) => {
+   const li = document.createElement('li');
+   li.textContent = beer.name;
+   ul.appendChild(li);
+   const image = document.createElement('img');
+   image.width = 200;
+   image.src = beer.image_url;
+   ul.appendChild(image);
+ });
+}
